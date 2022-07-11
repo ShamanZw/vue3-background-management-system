d6fd3f7866b6b001a78a331be0f96bfc8c9f8808
Zw <druidzw@163.com>
2022-07-11 20:50:08 +0800

    feat: 临时menu菜单

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="">Sidebar</div>
+  <div class="">
+    <h1>占位</h1>
+    <el-scrollbar>
+      <SidebarMenu></SidebarMenu>
+    </el-scrollbar>
+  </div>
 </template>
 
 <script setup>
 import {} from 'vue'
+import SidebarMenu from './SidebarMenu.vue'
 </script>
 
 <style lang="scss" scoped></style>
