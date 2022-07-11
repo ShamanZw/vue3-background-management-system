fbc835221b09f44f42131a4132e0be9d9e3cb04e
Zw <druidzw@163.com>
2022-07-11 15:47:51 +0800

    feat: 获取用户基本信息

--- src/permission.js
+++ src/permission.js
@@ -18,6 +18,10 @@ router.beforeEach(async (to, from, next) => {
     if (to.path === '/login') {
       next('/')
     } else {
+      // 判断用户资料是否存在，如果不存在，则获取用户信息
+      if (!store.getters.hasUserInfo) {
+        await store.dispatch('user/getUserInfo')
+      }
       next()
     }
   } else {
