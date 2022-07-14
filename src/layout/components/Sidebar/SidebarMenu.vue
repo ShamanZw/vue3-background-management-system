caede669781787ac47447e96b1db7677b0a511a3
Zw <druidzw@163.com>
2022-07-14 20:19:49 +0800

    feat: 处理menu样式、路由跳转、默认激活项

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -1,9 +1,11 @@
 <template>
   <el-menu
+    :default-active="activeMenu"
     :uniqueOpened="true"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b"
+    :background-color="$store.getters.cssVar.menuBg"
+    :text-color="$store.getters.cssVar.menuText"
+    :active-text-color="$store.getters.cssVar.menuActiveText"
+    router
   >
     <SidebarItem
       v-for="item in routes"
@@ -15,7 +17,7 @@
 
 <script setup>
 import { computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { filterRoutes, generateMenus } from '@/utils/route'
 import SidebarItem from './SidebarItem.vue'
 
@@ -23,6 +25,13 @@ const router = useRouter()
 const routes = computed(() => {
   const fRoutes = filterRoutes(router.getRoutes())
   return generateMenus(fRoutes)
+})
+
+// 默认激活
+const route = useRoute()
+const activeMenu = computed(() => {
+  const { path } = route
+  return path
 })
 </script>
 
