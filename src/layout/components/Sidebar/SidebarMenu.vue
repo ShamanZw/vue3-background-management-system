08baed48ec3368ae851de66c61693a3648e2cced
Zw <druidzw@163.com>
2022-07-14 20:07:39 +0800

    feat: 生成动态menu菜单

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -5,20 +5,11 @@
     text-color="#fff"
     active-text-color="#ffd04b"
   >
-    <!-- 子集 menu -->
-    <el-submenu index="1">
-      <template #title>
-        <i class="el-icon-location"></i>
-        <span>导航一</span>
-      </template>
-      <el-menu-item index="1-1">选项1</el-menu-item>
-      <el-menu-item index="1-2">选项2</el-menu-item>
-    </el-submenu>
-    <!-- 具体菜单 -->
-    <el-menu-item index="2">
-      <i class="el-icon-location"></i>
-      <template #title>导航4</template>
-    </el-menu-item>
+    <SidebarItem
+      v-for="item in routes"
+      :key="item.path"
+      :route="item"
+    ></SidebarItem>
   </el-menu>
 </template>
 
@@ -26,13 +17,13 @@
 import { computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { filterRoutes, generateMenus } from '@/utils/route'
+import SidebarItem from './SidebarItem.vue'
 
 const router = useRouter()
 const routes = computed(() => {
   const fRoutes = filterRoutes(router.getRoutes())
   return generateMenus(fRoutes)
 })
-console.log(JSON.stringify(routes.value))
 </script>
 
 <style lang="scss" scoped></style>
