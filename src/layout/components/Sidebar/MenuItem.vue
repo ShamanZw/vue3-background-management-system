0fcc2255a0800c3572d3dfeac086bb3b7b1777b4
Zw <druidzw@163.com>
2022-07-18 12:52:26 +0800

    feat: sidebar与面包屑区域国际化处理

--- src/layout/components/Sidebar/MenuItem.vue
+++ src/layout/components/Sidebar/MenuItem.vue
@@ -5,11 +5,12 @@
   <!-- <svg-icon v-else :icon="icon"></svg-icon> -->
   <svg-icon :icon="icon"></svg-icon>
   <!-- 文本 -->
-  <span>{{ title }}</span>
+  <span>{{ generateTitle(title) }}</span>
 </template>
 
 <script setup>
 import { defineProps } from 'vue'
+import { generateTitle } from '@/utils/i18n'
 defineProps({
   title: {
     type: String,
