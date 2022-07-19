277f2911986f87c234f5eb6918946b5a75e76d74
Zw <druidzw@163.com>
2022-07-19 14:49:04 +0800

    feat: 全屏功能实现

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -4,6 +4,7 @@
     <hamburger class="hamburger-container" />
     <breadcrumb class="breadcrumb-container" />
     <div class="right-menu">
+      <Screenfull class="right-menu-item hover-effect"></Screenfull>
       <ThemeSelect class="right-menu-item hover-effect"></ThemeSelect>
       <LangSelect class="right-menu-item hover-effect"></LangSelect>
       <!-- 头像 -->
@@ -42,6 +43,7 @@ import Hamburger from '@/components/Hamburger'
 import Breadcrumb from '@/components/Breadcrumb'
 import LangSelect from '@/components/LangSelect'
 import ThemeSelect from '@/components/ThemeSelect'
+import Screenfull from '@/components/Screenfull'
 import { Setting } from '@element-plus/icons-vue'
 
 const store = useStore()
